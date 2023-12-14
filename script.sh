12affbf2e05254252cd8a0acfffb8b9be69c4007
Lama-Khaled-Ismail <lamakhaledismail@gmail.com>
2023-12-14 17:46:38 +0200

    script working

--- script.sh
+++ script.sh
@@ -11,9 +11,10 @@ docker network connect kaf kafka1
 docker run -d -p 8080:80 --net front --name=angular_container angular_image:1.0
 docker run -d --name mongo_container -p 27017:27017 --net db mongo_image:1.0
 winpty docker exec -ti mongo_container mongosh localhost:27017 mongoScript.js
-winpty docker run -d -p 5000:5000 --net kaf --name=flaskapp flask_image:1.0
-winpty docker network connect db flaskapp
-winpty docker network connect front flaskapp
+sleep 1m
+docker run -d -p 5000:5000 --net kaf --name=flaskapp flask_image:1.0
+docker network connect db flaskapp
+docker network connect front flaskapp
 
 
 
